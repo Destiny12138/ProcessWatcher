7654f7396565448296612bc08e436183a91d6e5e
Johnny Zhou <1985219106@qq.com>
2026-08-31 10:22:50 +0800

    Initial Commit

---
+++ ProcessWatcher/ProcessWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProcessWatcherApp.swift
+//  ProcessWatcher
+//
+//  Created by 周光磊 on 2026/8/31.
+//
+
+import SwiftUI
+
+@main
+struct ProcessWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProcessWatcherTests/ProcessWatcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProcessWatcherTests.swift
+//  ProcessWatcherTests
+//
+//  Created by 周光磊 on 2026/8/31.
+//
+
+import Testing
+@testable import ProcessWatcher
+
+struct ProcessWatcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
